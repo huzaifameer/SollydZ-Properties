eed09bf00b425c4309a75aeb0fa1a570829cfdf6
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-12-24 22:14:31 +0530

    commit-4

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,11 +1,16 @@
 import HeroArea from "../HeroArea/HeroArea.jsx";
 import BannerArea from "../Banner/BannerArea.jsx";
+import {Link} from "react-router-dom";
 
 function HomePage(){
     return(
         <>
             <HeroArea hero='defaultHeroClass'>
-                <BannerArea mainTitle="Effortless Home Sales" subTitle='Unlocking Doors to Modern Transactions'/>
+                <BannerArea mainTitle="Effortless Home Sales" subTitle='Unlocking Doors to Modern Transactions'>
+                    <Link to='/properties' className='banner-btn'>
+                        Find Properties
+                    </Link>
+                </BannerArea>
             </HeroArea>
         </>
     )
--- src/Components/Properties/Properties.jsx
+++ src/Components/Properties/Properties.jsx
@@ -1,11 +1,16 @@
 import HeroArea from "../HeroArea/HeroArea.jsx";
 import BannerArea from "../Banner/BannerArea.jsx";
+import {Link} from "react-router-dom";
 
 function Properties(){
     return(
         <>
             <HeroArea hero='propertiesHero'>
-                <BannerArea mainTitle="Find Your Property" subTitle="Empowering Sellers, Delighting Buyers."/>
+                <BannerArea mainTitle="Find Your Property" subTitle="Empowering Sellers, Delighting Buyers.">
+                    <Link to='/' className='banner-btn'>
+                        Back to Home
+                    </Link>
+                </BannerArea>
             </HeroArea>
         </>
     )
