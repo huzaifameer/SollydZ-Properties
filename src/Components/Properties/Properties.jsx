816d1840b8ac64ccd326de7087ddcaea3ea062cb
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-12-26 13:14:11 +0530

    commit-5

--- src/Components/Properties/Properties.jsx
+++ src/Components/Properties/Properties.jsx
@@ -18,30 +18,8 @@ function Properties(){
             </HeroArea>
             <section className='services-area'>
                 <TitleBar title='Properties' />
-                {/*{propertyData.properties.map((property) => (
-                    <div key={property.id} className='container special-margin special-display'>
-                            <div className="card h-100" style={{ width: "18rem" }}>
-                                <img src={property.picture} className="card-img-top" alt="Property" />
-                                <div className="card-body">
-                                    <h5 className="card-title">{property.type}</h5>
-                                </div>
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">Price: ${property.price}</li>
-                                    <li className="list-group-item">Bedrooms: {property.bedrooms}</li>
-                                     Add more details as needed
-                                </ul>
-                                <div className="card-body">
-                                    <Link to={property.url} className="card-link">
-                                        <button className='btn btn-outline-primary'>View Details</button>
-                                    </Link>
-                                     Add more links as needed
-                                </div>
-                            </div>
-                    </div>
-
-                ))}*/}
+                <br/>
                 <PropertyCard/>
-
             </section>
         </>
     )
