eeb0639c9dc327e8f9903b07a73acfb7994250cb
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-12-30 19:23:20 +0530

    commit-9

--- src/Components/Properties/Properties.jsx
+++ src/Components/Properties/Properties.jsx
@@ -4,37 +4,92 @@ import TitleBar from "../TitleBar/TitleBar.jsx";
 import SingleProperty from "../SingleProperty/SingleProperty.jsx";
 import HeroArea from "../HeroArea/HeroArea.jsx";
 import BannerArea from "../Banner/BannerArea.jsx";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Properties extends Component {
     static contextType = PropertyContext;
 
+    state = {
+        favorites: [],
+    };
+
+    handleAddToFavorites = (propertyId) => {
+        // Check if the property is already in favorites
+        const isPropertyInFavorites = this.state.favorites.some((property) => property.id === propertyId);
+
+        if (!isPropertyInFavorites) {
+            // Find the selected property using the propertyId
+            const selectedProperty = this.context.properties.find((property) => property.id === propertyId);
+
+            // Add the property to favorites
+            this.setState((prevState) => ({
+                favorites: [...prevState.favorites, selectedProperty],
+            }));
+        }
+    };
+
+    handleRemoveFromFavorites = (propertyId) => {
+        // Remove the property from favorites
+        this.setState((prevState) => ({
+            favorites: prevState.favorites.filter((property) => property.id !== propertyId),
+        }));
+    };
+
     render() {
         let { loading, properties: properties } = this.context;
-        properties=properties.map(property =>{
-            return <SingleProperty key={property.id} property={property}/>
-        })
+        properties = properties.map((property) => {
+            const isInFavorites = this.state.favorites.some((favProperty) => favProperty.id === property.id);
+            return (
+                <SingleProperty
+                    key={property.id}
+                    property={property}
+                    onAddToFavorites={this.handleAddToFavorites}
+                    isInFavorites={isInFavorites}
+                    onRemoveFromFavorites={this.handleRemoveFromFavorites}
+                />
+            );
+        });
 
         return (
             <>
-                <HeroArea hero='propertiesHero'>
+                <HeroArea hero="propertiesHero">
                     <BannerArea mainTitle="Find Your Property" subTitle="Empowering Sellers, Delighting Buyers.">
-                        <Link to='/' className='banner-btn'>
+                        <Link to="/" className="banner-btn">
                             Back to Home
                         </Link>
                     </BannerArea>
                 </HeroArea>
-                <section className='services-area'>
-                    <section className='featured-properties'>
-                        <TitleBar title="Find Your Properties"/>
+                <section className="services-area">
+                    <section className="featured-properties">
+                        <TitleBar title="Find Your Properties" />
                         <div className="featured-properties-center">
-                            {loading ? <h1>Loading</h1>: properties}
+                            {loading ? <h1>Loading</h1> : properties}
                         </div>
                     </section>
                 </section>
 
-            </>
+                {/* Favorites Section */}
+                <section>
+                    <TitleBar title="Create Your Favorite Area"/>
+                    {this.state.favorites.map((favorite) => (
+                        <div key={favorite.id} className ='favorit-section'>
+                            <div  className="favorite-property">
+                                <img src={favorite.picture} alt={favorite.type} />
+                                <div>
+                                    <strong>{favorite.type}</strong> - ${favorite.price}
+                                </div>
+                                <button
+                                    className="remove-favorite-btn"
+                                    onClick={() => this.handleRemoveFromFavorites(favorite.id)}
+                                >
+                                    Remove from Favorites
+                                </button>
+                            </div>
+                        </div>
 
+                    ))}
+                </section>
+            </>
         );
     }
 }
--- src/Components/SingleProperty/SingleProperty.jsx
+++ src/Components/SingleProperty/SingleProperty.jsx
@@ -1,26 +1,38 @@
 import React from "react";
-import {Link} from 'react-router-dom'
-export default function SingleProperty({property}){
-    const {id,type,picture,price,bedrooms,tenure} =property;
+import { Link } from "react-router-dom";
+
+export default function SingleProperty({ property, onAddToFavorites, isInFavorites, onRemoveFromFavorites }) {
+    const { id, type, picture, price, bedrooms, tenure } = property;
+
     const handleViewDetails = () => {
-        history.push(`/properties/${tenure}`);
+        // Pass the property id and add/remove to favorites callback
+        if (isInFavorites) {
+            onRemoveFromFavorites(id);
+        } else {
+            onAddToFavorites(id);
+        }
     };
-    return(
+
+    return (
         <div className="card " style={{ width: "18rem" }}>
-            <img src={picture} className="card-img-top" style={{height:"200px"}} alt={type} />
-                <div className="card-body card-body-spcl">
-                    <h5 className="card-title">{type}</h5>
-                </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">Price : $ {price}</li>
-                    <li className="list-group-item">{bedrooms} Bedroom Residence</li>
-                </ul>
-                <div className="card-body card-body-spcl">
-                    <Link to={`/properties/${tenure}`} className="card-link btn btn-outline-dark" key={id} onClick={handleViewDetails}>View Details</Link>
-                </div>
-            <div className="favorite-btn">
-                Add to favorites
+            <img src={picture} className="card-img-top" style={{ height: "200px" }} alt={type} />
+            <div className="card-body card-body-spcl">
+                <h5 className="card-title">{type}</h5>
+            </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">Price : $ {price}</li>
+                <li className="list-group-item">{bedrooms} Bedroom Residence</li>
+            </ul>
+            <div className="card-body card-body-spcl">
+                <Link
+                    to={`/properties/${tenure}`}
+                    className="card-link btn btn-outline-dark">
+                    View Details
+                </Link>
+            </div>
+            <div className="favorite-btn" onClick={handleViewDetails}>
+                {isInFavorites ? "Remove from Favorites" : "Add to Favorites"}
             </div>
         </div>
-    )
+    );
 }
