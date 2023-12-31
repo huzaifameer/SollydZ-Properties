0f92c3ff508f88dea7030478416c1e2532a1995e
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-12-31 10:20:07 +0530

    commit-10

--- src/Components/SinglePage/SinglePage.jsx
+++ src/Components/SinglePage/SinglePage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, {useContext, useState} from "react";
 import { useParams } from 'react-router-dom';
 import { PropertyContext } from "../../context.jsx";
 
@@ -18,44 +18,74 @@ export default function SinglePage() {
     }
 
     const { type, description, bedrooms, price, location, images } = property;
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+    const handleImageClick = (index) => {
+        setSelectedImageIndex(index);
+    };
 
     return (
         <>
             <div className="container-fluid">
                 {/*image slider starting*/}
-                <div style={{backgroundColor:'#9FB3B4',borderRadius:'22px',padding:'10px',marginTop:'20px'}} id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <div style={{ backgroundColor: '#9FB3B4', borderRadius: '22px', padding: '10px', marginTop: '20px' }} id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-indicators">
                         {images && Array.isArray(images) && (
                             images.map((_, index) => (
-                                <button key={index} type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to={index} className={index === 0 ? 'active' : ''} aria-label={`Slide ${index + 1}`}></button>
+                                <button
+                                    key={index}
+                                    type="button"
+                                    data-bs-target="#carouselExampleCaptions"
+                                    data-bs-slide-to={index}
+                                    className={index === selectedImageIndex ? 'active' : ''}
+                                    aria-label={`Slide ${index + 1}`}
+                                    onClick={() => handleImageClick(index)}
+                                ></button>
                             ))
                         )}
                     </div>
                     <div className="carousel-inner">
                         {images && Array.isArray(images) && (
                             images.map((image, index) => (
-                                <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+                                <div key={index} className={`carousel-item ${index === selectedImageIndex ? 'active' : ''}`}>
                                     <img src={image} className="d-block w-50 mx-auto" alt={`Property ${index + 1}`} />
                                 </div>
                             ))
                         )}
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev" onClick={() => handleImageClick((selectedImageIndex - 1 + images.length) % images.length)}>
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Previous</span>
                     </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next" onClick={() => handleImageClick((selectedImageIndex + 1) % images.length)}>
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Next</span>
                     </button>
                 </div>
+                {/* Image slider ending */}
+
+                {/* Display small images below the slider */}
+                <div className="d-flex justify-content-center mt-3">
+                    {images && Array.isArray(images) && (
+                        images.map((image, index) => (
+                            <img
+                                key={index}
+                                src={image}
+                                className="img-thumbnail mx-2"
+                                alt={`Thumbnail ${index + 1}`}
+                                onClick={() => handleImageClick(index)}
+                                style={{ cursor: 'pointer', border: index === selectedImageIndex ? '1px solid #007BFF' : '2px solid transparent', width: '150px' }}
+                            />
+                        ))
+                    )}
+                </div>
                 {/*image slider ending*/}
                 {/* Displaying all the property details */}
                 <br/>
-                <h2 style={{textAlign:'center'}}>{type}</h2>
-                <p style={{paddingLeft:'50px'}}>Bedrooms -  {bedrooms}</p>
-                <p style={{paddingLeft:'50px'}}>Price - ${price}</p>
-                <p style={{paddingLeft:'50px'}}>Location - {location} {}</p>
+                <h2 style={{textAlign:'center'}}>{type}</h2>{/*property heading and type*/}
+                <p style={{paddingLeft:'50px'}}>Bedrooms -  {bedrooms}</p>{/*no of bedrooms*/}
+                <p style={{paddingLeft:'50px'}}>Price - ${price}</p>{/*prices of the property*/}
+                <p style={{paddingLeft:'50px'}}>Location - {location} {}</p>{/*property location*/}
                 <p style={{textAlign:'justify',padding:'40px 50px',backgroundColor:'#ffffff',borderRadius:'22px'}}>Description -  {description}</p>
                 <br/>
             </div>
