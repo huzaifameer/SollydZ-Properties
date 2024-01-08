2e1436d2d0ae862d2fba841d0f6a3f53653e6a8d
HuzaifaAmeer <huzaifaameer00@gmail.com>
2024-01-08 22:52:40 +0530

    commit-14-DateDone

--- src/Components/FilterProperty/FilterProperty.jsx
+++ src/Components/FilterProperty/FilterProperty.jsx
@@ -10,24 +10,25 @@ function FilterProperty({ onSearch }) {
         addedDate: "",
         bedRooms: "",
     });
-
+    /*function to handle the changes of the input and the state*/
     const handleTheChange = (event) => {
         setFilteredValues({
             ...filteredValues,
             [event.target.name]: event.target.value,
         });
     };
-
+    /*function to handle the search buttons' click event*/
     const handleTheSearch = () => {
         onSearch(filteredValues);
     };
 
     return (
-        <div className="container mt-4 border rounded p-4">
-            <h4 className="text-center text-light mb-4">Advance Your Searching</h4>
+        <div className="container mt-4 border rounded p-4">{/* creating a container with a fixed width*/}
+            <h4 className="text-center text-light mb-4">Advance Your Searching</h4>{/* Heading for the filter section */}
+            {/* Form for the property filtering process*/}
             <form>
-                <div className="row mb-3">
-                    <div className="col-12 col-sm-6 col-md-4 mb-3">
+                <div className="row mb-3">{/* First row of the filter option form */}
+                    <div className="col-12 col-sm-6 col-md-4 mb-3">{/* Dropdown for the property dropdown */}
                         <label htmlFor="type" className="form-label">Property Type</label>
                         <select name="type" id="type" className="form-select" onChange={handleTheChange}>
                             <option value="">All</option>
@@ -36,34 +37,42 @@ function FilterProperty({ onSearch }) {
                         </select>
                     </div>
 
+                    {/* Input option for the Location */}
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <label htmlFor="location" className="form-label">Location:</label>
                         <input type="text" name="location" id="location" className="form-control" onChange={handleTheChange} />
                     </div>
 
+                    {/* Input option for the Minimum Price */}
                     <div className="col-12 col-md-4 mb-3">
                         <label htmlFor="minPrice" className="form-label">Min Price:</label>
                         <input type="number" name="minPrice" id="minPrice" className="form-control" placeholder="Min Price" onChange={handleTheChange} />
                     </div>
                 </div>
 
-                <div className="row mb-3">
+                <div className="row mb-3">{/* Second row of the filter option form */}
+
+                    {/* Input option for the Maximum Price */}
                     <div className="col-12 col-md-3 mb-3">
                         <label htmlFor="maxPrice" className="form-label">Max Price:</label>
                         <input type="number" name="maxPrice" id="maxPrice" className="form-control" placeholder="Max Price" onChange={handleTheChange} />
                     </div>
 
+                    {/* Input option for the Added date */}
                     <div className="col-12 col-md-3 mb-3">
                         <label htmlFor="addedDate" className="form-label">Added Date:</label>
                         <input type="date" name="addedDate" id="addedDate" className="form-control" onChange={handleTheChange} />
                     </div>
 
+                    {/* Input option for the Number of bedrooms */}
                     <div className="col-12 col-md-3 mb-3">
                         <label htmlFor="bedRooms" className="form-label">No of Bed Rooms:</label>
                         <input type="number" name="bedRooms" id="bedRooms" className="form-control" onChange={handleTheChange} />
                     </div>
 
-                    <div className="col-12 col-md-3 d-flex ">
+                    {/* Search button for the filtering form */}
+                    <div className="col-12 col-md-3 {/*d-flex*/} ">
+                        <label htmlFor="nothing" className="form-label" style={{color:'white'}}><b>Find The Propert</b> </label>
                         <button type="button" className="btn btn-outline-light w-100" onClick={handleTheSearch}>
                             Search
                         </button>
